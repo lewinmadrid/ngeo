5a0f731e0227285632981a12bc357a02e6366c15
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-12-21 10:59:35 +0100

    istanbul

--- karma-conf-webpack.js
+++ karma-conf-webpack.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 var isDebug = process.argv.some(function(argument) {
     return argument === '--debug';
@@ -32,10 +33,12 @@ module.exports = function(config) {
 
     webpack: Object.assign({}, require('./buildtools/webpack.dev')),
 
+    plugins: ['karma-coverage-istanbul-reporter'],
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: isDebug ? ['progress'] : ['progress', 'coverage'],
+    reporters: isDebug ? ['progress'] : ['progress', 'coverage-istanbul'],
 
     // web server port
     port: 9876,
@@ -65,13 +68,38 @@ module.exports = function(config) {
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
-    coverageReporter: {
-      includeAllSources: true,
-      dir : '.build/coverage/',
-      reporters: [
-        {type: 'lcov', subdir: './'},
-        {type: 'text-summary', subdir: './', file: 'coverage.txt'}
-      ]
+    // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+    coverageIstanbulReporter: {
+
+       // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+      reports: ['lcovonly', 'text-summary'],
+
+       // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      dir: path.resolve(__dirname, '.build/coverage'),
+
+       // if using webpack and pre-loaders, work around webpack breaking the source path
+      fixWebpackSourcePaths: true,
+
+      // stop istanbul outputting messages like `File [${filename}] ignored, nothing could be mapped`
+      skipFilesWithNoCoverage: true,
+
+       // enforce percentage thresholds
+       // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
+        global: { // thresholds for all files
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        },
+        each: { // thresholds per file
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        }
+      }
     }
   });
 };
